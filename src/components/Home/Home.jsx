b3464a0f7cd6521c701c36176cf5fa9e921af8e2
pujaKundu <pujakundu797@gmail.com>
2024-01-18 13:42:27 +0600

    hover

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -11,6 +11,7 @@ const Home = () => {
   const [tooltipPosition, setTooltipPosition] = useState({ x: 0, y: 0 });
   const [dragging, setDragging] = useState(false);
   const [tooltipVisible, setTooltipVisible] = useState(false);
+  const [isInnerDivHovered, setIsInnerDivHovered] = useState(false);
 
   // console.log(position);
   // console.log(dragging);
@@ -25,8 +26,14 @@ const Home = () => {
           const newX = e.clientX - containerRect.left - 25;
           const newY = e.clientY - containerRect.top - 25;
 
-          const clampedX = Math.max(0, Math.min(newX, containerRect.width - 100));
-          const clampedY = Math.max(0, Math.min(newY, containerRect.height - 50));
+          const clampedX = Math.max(
+            0,
+            Math.min(newX, containerRect.width - 100)
+          );
+          const clampedY = Math.max(
+            0,
+            Math.min(newY, containerRect.height - 50)
+          );
 
           setPosition({ x: clampedX, y: clampedY });
         }
@@ -54,27 +61,55 @@ const Home = () => {
     positionRef.current = newPosition;
 
     setTooltipVisible(true);
+    setIsInnerDivHovered(true);
   };
 
   const handleMouseEnter = () => {
-    if (!dragging && (position.x !== positionRef.current.x || position.y !== positionRef.current.y)) {
+    if (
+      !dragging &&
+      (position.x !== positionRef.current.x ||
+        position.y !== positionRef.current.y)
+    ) {
       setTooltipVisible(true);
     }
+
+    setIsInnerDivHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    if (!dragging) {
+      setTooltipVisible(false);
+    }
+
+    setIsInnerDivHovered(false);
   };
 
   const tooltipStyle = {
-    top: `${position.y >= 0 && position.y <= 60
-      ? tooltipPosition.y + (containerRef?.current?.getBoundingClientRect()?.top || 0) + 60
-      : tooltipPosition.y + (containerRef?.current?.getBoundingClientRect()?.top || 0) - 40
+    top: `${
+      position.y >= 0 && position.y <= 60
+        ? tooltipPosition.y +
+          (containerRef?.current?.getBoundingClientRect()?.top || 0) +
+          60
+        : tooltipPosition.y +
+          (containerRef?.current?.getBoundingClientRect()?.top || 0) -
+          40
     }px`,
-    left: `${tooltipPosition.x + (containerRef?.current?.getBoundingClientRect()?.left-5 || 0)}px`,
-    display: tooltipVisible ? "block" : "none",
+    left: `${
+      tooltipPosition.x +
+      (containerRef?.current?.getBoundingClientRect()?.left - 5 || 0)
+    }px`,
+    display: tooltipVisible && isInnerDivHovered ? "block" : "none",
   };
 
   return (
     <>
       {createPortal(
-        <div className={`tooltip `} style={tooltipStyle}>
+        <div
+          className={`tooltip `}
+          style={tooltipStyle}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
           This is tooltip
         </div>,
         document.body
@@ -90,6 +125,7 @@ const Home = () => {
           style={{ top: `${position.y}px`, left: `${position.x}px` }}
           onMouseDown={handleMouseDown}
           onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
           ref={innerDivRef}
         ></div>
       </div>
